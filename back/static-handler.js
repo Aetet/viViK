9aeb653c9e7e77a03edccced6dec3264ee07e199
Aetet <Aetetic@gmail.com>
2013-08-16 00:19:10 +0400

    static handler

--- back/static-handler.js
+++ back/static-handler.js
@@ -1,5 +1,5 @@
 var static = require('node-static');
-var fileServer = new static.Server('../public');
+var fileServer = new static.Server('../public/build');
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
